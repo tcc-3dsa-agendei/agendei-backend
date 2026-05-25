04630fa257d17ad282099a4f02858de1a3f17a48
Vitor Felicio <desenvolvedorvitor67@gmail.com>
2026-05-25 20:49:33 -0300

    Remoção do logger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
-import { logger } from "@/lib/logger"
 import { sendWhatsappMessage } from "@/routes/whatsapp-message"
 import cors from "@elysiajs/cors"
-import consola from "consola"
 import Elysia from "elysia"
 
 export const app = new Elysia().use(
@@ -14,6 +12,6 @@ export const app = new Elysia().use(
   })
     .use(sendWhatsappMessage)
     .listen(3333, ({ url }) => {
-      logger.error(`Servidor rodando: ${url}`)
+      console.log(`Servidor rodando: ${url}`)
     })
 )
